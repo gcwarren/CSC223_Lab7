c60c257a1dcfac378fcc7a7c0ad785c77636bd2b
gcwarren <grace.warren@furman.edu>
2023-10-31 22:46:03 -0400

    some tests

--- test/geometry_objects/PointNamingFactoryTest.java
+++ test/geometry_objects/PointNamingFactoryTest.java
@@ -1,9 +1,7 @@
 package geometry_objects;
 
 import static org.junit.Assert.*;
-
 import org.junit.jupiter.api.Test;
-
 import geometry_objects.points.Point;
 import geometry_objects.points.PointNamingFactory;
 
@@ -55,37 +53,88 @@ public class PointNamingFactoryTest {
 	
 	@Test
 	void getPointTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
+		assertEquals(PNF.get(point1), point1);
+		assertEquals(PNF.get(point2), point2);
+		assertEquals(PNF.get(point3), point3);
 	}
 	
 	@Test 
 	void getDoubleTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
+		assertEquals(PNF.get(2.0, 5.0), point1);
+		assertEquals(PNF.get(3.0, 6.0), point2);
+		assertEquals(PNF.get(4.0, 7.0), point3);
 	}
 	
 	@Test 
 	void containsPointTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
+		assertTrue(PNF.contains(point1));
+		assertTrue(PNF.contains(point2));
+		assertTrue(PNF.contains(point3));
 	}
 	
 	@Test 
 	void containsDoubleTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
-	}
-	
-	@Test 
-	void getCurrentNameTest() {
-		
+		assertTrue(PNF.contains(2.0, 5.0));
+		assertTrue(PNF.contains(3.0, 6.0));
+		assertTrue(PNF.contains(4.0, 7.0));
 	}
 	
 	@Test 
 	void getAllPointsTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
+		assertEquals(PNF.getAllPoints().size(), 3);
 	}
 	
 	@Test 
 	void clearTest() {
+		PointNamingFactory PNF = new PointNamingFactory();
+		Point point1 = new Point(2.0, 5.0);
+		Point point2 = new Point(3.0, 6.0);
+		Point point3 = new Point(4.0, 7.0);
+		PNF.put(point1); 
+		PNF.put(point2);
+		PNF.put(point3);
 		
+		assertNull(PNF.clear());
 	}
 	
 	@Test 
