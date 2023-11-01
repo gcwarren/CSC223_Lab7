1edb9cff7bed28418cba1cfcd435409d1cf18b19
gcwarren <grace.warren@furman.edu>
2023-10-31 20:04:25 -0400

    another update

--- src/input/InputFacade.java
+++ src/input/InputFacade.java
@@ -3,6 +3,8 @@ package input;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -27,35 +29,35 @@ public class InputFacade
 	 *     Constructs a parser
 	 *     Acquries an input file string.
 	 *     Parses the file.
-     *
+	 *
 	 * @param filepath -- the path/name defining the input file
 	 * @return a FigureNode object corresponding to the input file.
 	 */
 	public static FigureNode extractFigure(String filepath)
 	{
-//        // TODO
-//		//so are we calling parser on a JSON object?
-//		//call the file we want to read and then store the info into a JSON object?
-//		JSONParser fileParser = new JSONParser(new GeometryBuilder());
-//		//THIS IS WRONG...you need to find the file then read
-//		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filepath); 
-//		JSONParser parser = parser.parse(figureStr);
-//		return toGeometryRepresentation(fileParser.);
-		
-		
+		//        // TODO
+		//		//so are we calling parser on a JSON object?
+		//		//call the file we want to read and then store the info into a JSON object?
+		//		JSONParser fileParser = new JSONParser(new GeometryBuilder());
+		//		//THIS IS WRONG...you need to find the file then read
+		//		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filepath); 
+		//		JSONParser parser = parser.parse(figureStr);
+		//		return toGeometryRepresentation(fileParser.);
+
+
 		JSONParser parser = new JSONParser(new GeometryBuilder());
 
 		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filepath);
-						
+
 		return (FigureNode) parser.parse(figureStr);
 
 	}
-	
+
 	/**
 	 * 1) Convert the PointNode and SegmentNode objects to a Point and Segment objects 
 	 *    (those classes have more meaningful, geometric functionality).
 	 * 2) Return the points and segments as a pair.
-     *
+	 *
 	 * @param fig -- a populated FigureNode object corresponding to a geometry figure
 	 * @return a point database and a set of segments
 	 */
@@ -64,15 +66,13 @@ public class InputFacade
 		// TODO IS MAP SUPPOSE TO CONTAIN INTERABLE COMPOENETS OR DO WE JUST PASS VALUES
 		PointNodeDatabase pndb = fig.getPointsDatabase();//convert from pointNodeDB to pointdatabase
 		SegmentNodeDatabase sndb = fig.getSegments(); //get all segemnts w/ helper methods
-		
-		return new Map.Entry<PointDatabase, Set<Segment>>(convertToPoints(pndb), convertToSegments(sndb, convertToPoints(pndb)));
-	
+
+		return new AbstractMap.SimpleEntry<PointDatabase, Set<Segment>>(convertToPoints(pndb), convertToSegments(sndb, convertToPoints(pndb)));
 	}
 	//use a convert method 
 
-    //	
+	//	
 	// TODO: implement other support methods to facilitate the toGeometryRepresentation method
-	// like WHAT???
 	private PointDatabase convertToPoints(PointNodeDatabase pndb) {
 		PointDatabase newPD = new PointDatabase();
 		List<String> pndbNameList = pndb.getAllNodeNames();
@@ -83,17 +83,17 @@ public class InputFacade
 		}
 		return newPD;
 	}
-	
-	private Segment convertToSegments(SegmentNodeDatabase sndb, PointDatabase pndb) {
-		PointDatabase newPD = new PointDatabase();
-		List<String> pndbNameList = pndb.getAllNodeNames();
-		for (String name: pndbNameList) {
-			PointNode node = pndb.getNodeByName(name);
-			List<String> edgeList = sndb.edgesAsList(node);
-			for (String segName : edgeList) {
-				new Segment(node, pndb.getNodeByName(segName));
+
+	private Set<Segment> convertToSegments(SegmentNodeDatabase sndb, PointDatabase pdb) {
+		Set<Segment> segments = new HashSet<Segment>();
+		
+		for (SegmentNode segNode: sndb.asSegmentList()) {
+			Point point1 = new Point(segNode.getPoint1().getX(), segNode.getPoint1().getY());
+			Point point2 = new Point(segNode.getPoint2().getX(), segNode.getPoint2().getY());
+			if (pdb.getPoint(point1) != null & pdb.getPoint(point2) != null) {
+				segments.add(new Segment(point1, point2));
 			}
 		}
-						
+		return segments;
 	}
 }
