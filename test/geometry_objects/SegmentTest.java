a8c4c049359ff0a864c51f9a21abbcd4749ba132
MengsrunNit <117285282+MengsrunNit@users.noreply.github.com>
2023-11-09 15:46:23 -0500

    testing

--- test/geometry_objects/SegmentTest.java
+++ test/geometry_objects/SegmentTest.java
@@ -1,5 +1,7 @@
 package geometry_objects;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashSet;
 import geometry_objects.points.*;
 
@@ -8,24 +10,36 @@ import org.junit.jupiter.api.Test;
 public class SegmentTest {
 	
 	public Segment builder() {
-		Segment _segment = new Segment(new Point("A", 1,1), new Point("B", 5,4));
+		Segment _segment = new Segment(new Point("A", 1,2), new Point("B", 5,4));
 		
 		return _segment;
 	}
 	
 	@Test 
-	public hasSubSegmentTest() {
+	public void hasSubSegmentTest() {
+		Segment segment = builder(); 
+		
+		Point pointC = new Point("C", 1, 2);
+		
+		Segment segment1 = new Segment(pointC, new Point("B", 5,4)); 
+		Segment segment2 = new Segment(new Point("D", 3,3), new Point("E", 2, 2.5));
+		
+		
+		assertTrue(segment.HasSubSegment(segment1));
+		assertTrue(segment.HasSubSegment(segment2));
+
+		
 		
 		
 	}
 	
 	@Test
-	public coincideWithoutOverlapTest() {
+	public void coincideWithoutOverlapTest() {
 		
 	}
 	
 	@Test
-	public collectOrderedPointsOnSegmentTest() {
+	public void collectOrderedPointsOnSegmentTest() {
 		
 		Set<Point> givenPoints = new HashSet<Point>();
 		
