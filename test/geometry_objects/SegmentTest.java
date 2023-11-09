c5b4146827c4073777176750278825b77c5741db
gcwarren <grace.warren@furman.edu>
2023-11-09 15:21:41 -0500

    test headers

--- test/geometry_objects/SegmentTest.java
+++ test/geometry_objects/SegmentTest.java
@@ -1,5 +1,20 @@
 package geometry_objects;
 
 public class SegmentTest {
+	
+	@Test 
+	public hasSubSegmentTest() {
+		
+	}
+	
+	@Test
+	public coincideWithoutOverlapTest() {
+		
+	}
+	
+	@Test
+	public collectOrderedPointsOnSegmentTest() {
+		
+	}
 
 }
