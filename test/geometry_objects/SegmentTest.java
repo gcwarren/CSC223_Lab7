43107d71925f6afdd7983b52c35bb75beca2d333
MengsrunNit <117285282+MengsrunNit@users.noreply.github.com>
2023-11-09 15:29:28 -0500

    builder() testing

--- test/geometry_objects/SegmentTest.java
+++ test/geometry_objects/SegmentTest.java
@@ -1,13 +1,21 @@
 package geometry_objects;
 
 import java.util.HashSet;
+import geometry_objects.points.*;
 
 import org.junit.jupiter.api.Test;
 
 public class SegmentTest {
 	
+	public Segment builder() {
+		Segment _segment = new Segment(new Point("A", 1,1), new Point("B", 5,4));
+		
+		return _segment;
+	}
+	
 	@Test 
 	public hasSubSegmentTest() {
+		
 		
 	}
 	
